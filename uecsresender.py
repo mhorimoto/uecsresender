857c7393b293cd30a6a4c686978dc91ab6f6ee25
Masafumim Horimoto on smena8 <masafumi.horimoto@gmail.com>
2024-01-15 09:01:23 +0900

    とりあえず完成版

--- uecsresender.py
+++ uecsresender.py
@@ -2,22 +2,50 @@
 #code: utf-8
 
 import sys
+import time
 import datetime
+import socket
+import netifaces
 
 lcnt=0   # line counter
-lmax=100 # default line max limit
 lbas=0   # line buffer TOD base
-dift=0   #
+repoch=0 # Epoch time diff in record
+cepoch=0 # Current epoch time
+i     =0
 
-lbuf = [ input() for i in range(lmax) ]
+IFACE='enp4s0'
 
-for i in range(lmax):
+ADDRESS = netifaces.ifaddresses(IFACE)[netifaces.AF_INET][0]['broadcast']
+PORT = 16520
+DEST_ADDR = (ADDRESS,PORT)
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
+s.bind((ADDRESS,PORT))
+
+lbuf = []
+for l in sys.stdin:
+    lbuf.append(l.rstrip())
+
+lmax = len(lbuf)
+
+i=0
+
+while True:
     tm = lbuf[i][0:8]
     dt = lbuf[i][9:19]
     msg= lbuf[i][20:]
     tod= dt+" "+tm
     date = datetime.datetime.strptime(tod, '%Y-%m-%d %H:%M:%S').timestamp()
     if i==0:
+        print("set base val")
         lbas = date
-    dift = (int)(date - lbas)
-    print("{0} {1}".format(dift,msg))
+        bepoch = int(time.time())
+    cepoch = int(time.time()) - bepoch
+    repoch = (int)(date - lbas)
+    if (repoch<=cepoch):
+        s.sendto(msg.encode("utf-8"),DEST_ADDR)
+        print("i={0}  Cepoch={1}  Repoch={2} {3} {4}".format(i,cepoch,repoch,tm,msg))
+        i+=1
+        if (i>=lmax):
+            break
+
